35d0e9d4a11abcaf2f48dea44f34b88793fd3875
Shortytheman <shor7ty@gmail.com>
2023-03-01 15:20:25 +0100

    j

---
+++ ConsoleApp5/opgave5.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp5;
+
+public class opgave5
+{
+
+    
+
+
+    public static void ModifyValue(ref int a, in int b, out int c)
+    {
+        a += b;
+        c = a - b;
+    }
+
+    public static void Main(string[] args)
+    {
+        int x = 10;
+
+        int y = 5;
+
+        int z;
+        
+        Console.WriteLine($"Before calling ModifyValue: x={x}, y={y}");
+        
+        ModifyValue(ref x, y, out z);
+        
+        Console.WriteLine($"After calling ModifyValue: x={x}, z={z}");
+    }
+
+}
