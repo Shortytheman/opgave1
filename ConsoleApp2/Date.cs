2ab08f0616c546bafaa02ed7ae64acb4aa00deee
Shortytheman <shor7ty@gmail.com>
2023-03-01 14:48:58 +0100

    asddasasd

---
+++ ConsoleApp2/Date.cs
@@ -0,0 +1,49 @@
+﻿namespace ConsoleApp2;
+
+public struct Date
+{
+    private int year;
+    private int month;
+    private int day;
+
+    public Date(int year, int month, int day)
+    {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+
+    public bool IsValid()
+    {
+        bool IsValid = true;
+        try
+        {
+            DateTime dateTime1 = new DateTime(year, month, day);
+        }
+        catch (Exception)
+        {
+            IsValid = false;
+            Console.WriteLine("The date is not valid..");
+        }
+
+        return IsValid;
+    }
+
+    public override string ToString()
+    {
+        return $"Day: {day}, Month: {month}, Year: {year}";
+    }
+
+    public static void Main(string[] args)
+    {
+        Date date1 = new Date(2023, 2, 17);
+        Date date2 = new Date(2023, 2, 29);
+        Date date3 = new Date(2023, 13, 1);
+
+
+        Console.WriteLine("Date 1 is valid? " + date1.IsValid());
+        Console.WriteLine("Date 2 is valid? " + date2.IsValid());
+        Console.WriteLine("Date 3 is valid? " + date3.IsValid());
+    }
+}
---
+++ ConsoleApp3/Person.cs
@@ -0,0 +1,86 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleApp3;
+
+public class Person
+{
+    [StringLength(100), Required]
+    public string Name { get; set; }
+
+    [Range(0, 120), Required] public int Age { get; set; }
+
+    [EmailAddress]
+    public string? Email { get; set; }
+
+    public Person(string name, int age, string email)
+    {
+        Name = name;
+        Age = age;
+        Email = email;
+    }
+    
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+    
+
+    public static void Main(string[] args)
+    {
+        var person1 = new Person("John", 30, "john@example.com");
+        Console.WriteLine(person1.Validate()); // no exception
+        
+        
+        var person2 = new Person(null, 150);
+        try
+        {
+            Console.WriteLine(person2.Validate());
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message); // "Name must be between 1 and 100 characters."
+        }
+
+ 
+
+        var person3 = new Person("Jane", -10, "jane@example.com");
+        try
+        {
+            Console.WriteLine(person3.Validate());
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message); // "Age must be between 0 and 120."
+        }
+
+ 
+
+        var person4 = new Person("Bob", 40, "invalid-email");
+
+        try
+
+        {
+
+            Console.WriteLine(person4.Validate());
+
+        }
+
+        catch (ArgumentException ex)
+
+        {
+
+            Console.WriteLine(ex.Message); // "Email must be a valid email address format."
+
+        }
+    }
+    public bool Validate()
+    {
+        Console.WriteLine("Name: " + Name + " Age: " + Age + " Email: " + Email);
+        Person person = new Person(Name,Age,Email);
+        var result = new List<ValidationResult>();
+        var context = new ValidationContext(person);
+        return Validator.TryValidateObject(person, context, result, true);
+    }
+
+}
